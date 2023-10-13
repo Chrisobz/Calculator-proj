26d831847eca67764b441fda6659a7317c439256
chris <chris@LAPTOP-B53DSNT6>
2023-10-13 17:33:08 +0100

    added a text field for calculator

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ def evaluate_calculation():
 def clear_field():
     pass
 
+root = tk.Tk()
+root.geometry("300x375")
+
+#text field 
+
+text_result = tk.Text(root, height=2, width=16, font=("Arial" , 25))
+text_result.grid(columnspan=5)
+root.mainloop()
